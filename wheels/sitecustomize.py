332476cb49fcc38114f855e57bf2c74f7a0e1960
Ashton Dong <dong.ashton@gmail.com>
2026-04-15 02:05:51 -0400

    added dlib w/ cuda support(UV only)
    NOTE: dlib must be compiled from source to enable cuda support -
    requires attached sitecustomize under site-packages to link external
    dlls.

---
+++ wheels/sitecustomize.py
@@ -0,0 +1,4 @@
+import os
+os.add_dll_directory(r'C:\Program Files\NVIDIA\CUDNN\v9.21\bin\13.2\x64')
+os.add_dll_directory(r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v13.2\bin\x64')
+os.add_dll_directory(r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v13.2\bin')
