6fd2219a5df57f20c70e8c9359af12c25767e15b
Ashton Dong <dong.ashton@gmail.com>
2026-04-15 13:26:36 -0400

    minor improvements and bugfixes
    - remove double normalization(F.normalizeZ(embv, dim=1)
    - added batch handling
    - IMPORTANT: ADDED LABEL DECODER(INT -> LABEL)

--- code/face_finder_core/ocnn_classifier.py
+++ code/face_finder_core/ocnn_classifier.py
@@ -5,41 +5,52 @@
 ISSUES: query + embedding db needs to be normalized vectors
 """
 
+import pickle
+
 import torch
 
 from .ocnn_model import build_model
 
 
 class FaceClassifier:
-    def __init__(self, model_path, db_path, device="cuda"):
+    def __init__(self, model_path, db_path, label_encoder_path, device="cuda"):
         self.device = device
-        self.model = build_model(model_path)
-        self.model.load_state_dict(torch.load(model_path))
+
+        # Model
+        self.model = build_model(device)
+        self.model.load_state_dict(torch.load(model_path, map_location=device))
         self.model.eval()
 
+        # Database
         self.db_embeddings, self.db_labels = self.load_database(db_path)
+        self.db_embeddings = self.db_embeddings.to(device)
+        self.db_labels = self.db_labels.to(device)
+
+        # Label encoder
+        with open(label_encoder_path, "rb") as f:
+            self.label_encoder = pickle.load(f)
 
     def load_database(self, path):
         data = torch.load(path)
         return data["embeddings"], data["labels"]
 
-    # still in progess - needs MTCNN/RetinaFace to get face before model runs on it
     def get_embedding(self, image):
         with torch.no_grad():
-            emb = self.model(image.to(self.device))
-            emb = torch.nn.functional.normalize(emb, dim=1)
+            image = image.to(self.device)
+            emb = self.model(image)  # already normalized
         return emb
 
     def recognize(self, image, threshold=0.4):
         emb = self.get_embedding(image)
 
         sims = torch.matmul(emb, self.db_embeddings.T)
-        best_idx = sims.argmax()
 
-        confidence = sims[0, best_idx].item()
-        label = self.db_labels[best_idx]
+        best_idx = sims.argmax(dim=1)
+        confidence = sims.max(dim=1).values.item()
+        label = self.db_labels[best_idx].item()
 
         if confidence < threshold:
             return "unknown", confidence
 
-        return label, confidence
+        name = self.label_encoder.inverse_transform([label])[0]
+        return name, confidence
